d7fd19de520628b2555ba1b60d53facc6a4e5891
Jim Cheung <usr.jim@gmail.com>
2014-09-24 11:09:28 +0800

    updated bootstrap

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -3,7 +3,10 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y lxc-docker vim wget curl telnet golang samba
+apt-get install -y lxc-docker vim git wget curl telnet golang samba
+
+echo "Asia/Hong_Kong" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 
 # -----
 # samba
